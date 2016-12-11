7dcb3b6d4defc902d7ade56d15c1baf8a8fa0a2d
TK <ngtiankiat97@gmail.com>
2016-12-11 00:54:46 -0800

    Base Project Commit
    Commit the base project.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,121 @@
+﻿using System.IO;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameController : MonoBehaviour {
+	[SerializeField]
+	private Spawner[] spawners;
+	[SerializeField]
+	private int[] wavesInfo; // array of numbers that say how many balls to spawn
+	public int waveNumber = 0;
+	[SerializeField]
+	private Text countDownText;
+
+	public bool DebugMode;
+	// Use this for initialization
+	void Start ()
+	{
+		wavesInfo = ReadWavesFile("Assets/Levels.txt");
+		StartCoroutine(GameLoop());
+	}
+
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.D)) {
+			DebugMode = !DebugMode;
+			GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>().isTrigger = DebugMode;
+			GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>().isKinematic = DebugMode;
+		}
+	}
+
+	IEnumerator GameLoop()
+	{
+		// count down
+		countDownText.text = "3";
+		yield return new WaitForSeconds(1);
+		countDownText.text = "2";
+		yield return new WaitForSeconds(1);
+		countDownText.text = "1";
+		yield return new WaitForSeconds(1);
+		countDownText.text = "";
+		SpawnWave();
+
+		while (waveNumber < wavesInfo.Length) 
+		{
+			if (GameObject.FindGameObjectsWithTag("Ball").Length == 0) 
+			{
+				//count down
+				countDownText.text = "3";
+				yield return new WaitForSeconds(1);
+				countDownText.text = "2";
+				yield return new WaitForSeconds(1);
+				countDownText.text = "1";
+				yield return new WaitForSeconds(1);
+				countDownText.text = "";
+				SpawnWave();
+			}
+			if (DebugMode && Input.GetKeyDown(KeyCode.K)) 
+			{
+				GameObject[] temp = GameObject.FindGameObjectsWithTag("Ball");
+				foreach (var item in temp) 
+				{
+					item.GetComponent<Ball>().Split();
+				}
+			}
+			yield return null;
+		}
+
+	}
+
+	void SpawnWave()
+	{
+		// for now spawn from the only spawner
+		spawners[0].SpawnBalls(wavesInfo[waveNumber]);
+		waveNumber++; // increase wave number
+	}
+
+	int[] ReadWavesFile(string fileName)
+	{
+		string line;
+		try 
+		{
+			StreamReader theReader = new StreamReader(fileName, true);
+			using (theReader)
+	        {
+	         // While there's lines left in the text file, do this:
+	         do
+	         {
+	             line = theReader.ReadLine();
+	                 
+	             if (line != null)
+	             {
+	                 // Do whatever you need to do with the text line, it's a string now
+	                 // In this example, I split it into arguments based on comma
+	                 // deliniators, then send that array to DoStuff()
+	                 string[] entries = line.Split(',');
+	                 if (entries.Length > 0)
+	                 {
+	                 	int[] result = new int[entries.Length];
+						for (int i = 0; i < entries.Length; i++) 
+						{
+							result[i] = int.Parse(entries[i]);
+						}
+						return result;
+	                 }
+
+	             }
+	         }
+	         while (line != null);
+	         // Done reading, close the reader and return true to broadcast success    
+	         theReader.Close();
+	     }
+
+	    }
+		catch (System.Exception ex) 
+		{
+			Debug.Log(ex.Message);
+			return null;
+		}
+		return null;
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+	private Vector2 origin;
+
+	[SerializeField]
+	private float width; // width of spawning box
+	[SerializeField]
+	private float height; // height of spawning box
+	[SerializeField]
+	private float minHeight; // minimum height from bottom of spawn box to spawn the balls
+	[SerializeField]
+	private float minSpacing; // minimum spacing between the balls that spawn
+	[SerializeField]
+	private GameObject[] spawnables; // balls that this spawner can spawn
+
+	private SpawningBox spawnBox; // spawn box instance
+	// Use this for initialization
+	void Start () 
+	{
+		origin = transform.position;
+		spawnBox = CreateSpawnBox(width, height); // initialize the spawn box
+	}
+	// Method: spawn a given number of balls
+	public void SpawnBalls(int _numOfBalls)
+	{
+		Vector2 lastSpawnPosition = origin; // position of ball that was last spawned
+		for (int i = 0; i < _numOfBalls; ++i) 
+		{
+			// generate random position within the spawnBox
+			Vector2 spawnPos = new Vector2(Random.Range(spawnBox.botLeft.x, spawnBox.BotRight.x),Random.Range(spawnBox.botLeft.y, spawnBox.topLeft.y));
+			// if the spawn position is far enough from the last spawn position and if it is above the minimum height then spawn the ball
+			if (Vector2.Distance(spawnPos, lastSpawnPosition) > minSpacing) 
+			{
+				// spawn a random ball
+				int randomIndex = Mathf.RoundToInt(Random.Range(0, spawnables.Length));
+				Mathf.Clamp(randomIndex, 0, spawnables.Length - 1);
+
+				GameObject temp = Instantiate(spawnables[randomIndex], spawnPos, Quaternion.identity) as GameObject;
+				temp.GetComponent<Ball>().startForce = i % 2 == 0 ? (Vector2.right * 2) :(Vector2.left * 2);
+
+				// update last spawn position
+				lastSpawnPosition = spawnPos;
+			} else 
+			{
+				// decrease the iterator to try again
+				--i;
+			}
+		}
+
+	}
+
+	// Method: create spawning box
+	private SpawningBox CreateSpawnBox(float _width, float _height)
+	{
+		// calculate corners
+		Vector2 v1 = new Vector2(origin.x - _width / 2, origin.y + _height / 2);
+		Vector2 v2 = new Vector2(origin.x + _width / 2, origin.y + _height / 2);
+		Vector2 v3 = new Vector2(origin.x - _width / 2, origin.y - _height / 2);
+		Vector2 v4 = new Vector2(origin.x + _width / 2, origin.y - _height / 2);
+
+		return new SpawningBox(v1, v2, v3, v4);
+	}
+	// Method: for drawing a visual representation of the spawning box when in the editor
+	void OnDrawGizmos()
+	{
+		// if width or height is more than 0 create a visual representation of the spawning box
+		if (width > 0 || height > 0 && !Application.isPlaying) 
+		{
+			origin = transform.position;
+			Gizmos.color = Color.blue;
+			SpawningBox sb = CreateSpawnBox(width, height);
+			Gizmos.DrawLine(sb.topLeft, sb.topRight);
+			Gizmos.DrawLine(sb.topLeft, sb.botLeft);
+			Gizmos.DrawLine(sb.topRight, sb.BotRight);
+			Gizmos.DrawLine(sb.botLeft, sb.BotRight);
+		}
+	}
+
+	// Struct: spawning box
+	struct SpawningBox
+	{
+		public Vector2 topLeft, topRight, botLeft, BotRight;
+		public SpawningBox (Vector2 _topleft, Vector2 _topRight, Vector2 _botLeft, Vector2 _botRight)
+		{
+			topLeft = _topleft;
+			topRight = _topRight;
+			botLeft = _botLeft;
+			BotRight = _botRight;
+		}
+	}
+}
